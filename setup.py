692b8d46e6bb535ede103c29696a9172814af563
Andrei Dragomir <adragomi@adobe.com>
2015-11-07 17:40:09 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='tropopause',
+      version='0.1',
+      description='Extra utilities for troposphere',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: CloudTools :: Linguistic',
+      ],
+      url='http://github.com/adragomir/tropopause',
+      author='Andrei Dragomir',
+      author_email='adragomir@gmail.com',
+      license='MIT',
+      packages=['tropopause'],
+      install_requires=[
+          'troposphere',
+          'awacs'
+      ],
+      zip_safe=True)
